ea85e5d430c8759783dfa5be3d8fa4f3987c20b3
coderyeasin <yeasin3233@gmail.com>
2026-01-18 00:07:44 +0600

    bst-off

--- src/components/BestOffer/BestOffer.tsx
+++ src/components/BestOffer/BestOffer.tsx
@@ -1,10 +1,18 @@
-import React from "react";
+import Container from "../layout/Container";
+import Section from "../layout/Section";
 
 const BestOffer = () => {
   return (
-    <div>
-      <h3>BestOffer</h3>
-    </div>
+    <Container>
+      <Section>
+        <h3>
+          FIrst <section></section>
+        </h3>
+      </Section>
+      <Section>
+        <h3>Section</h3>
+      </Section>
+    </Container>
   );
 };
 
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -3,6 +3,7 @@ import Section from "../layout/Section";
 import Container from "../layout/Container";
 import Offer from "../Offer/Offer";
 import WelcomeParty from "../WelcomeParty/WelcomeParty";
+import BestOffer from "../BestOffer/BestOffer";
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
         <Banner />
         <Offer />
         <WelcomeParty />
+        <BestOffer />
       </Container>
     </Section>
   );
