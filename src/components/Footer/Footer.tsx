216ddac2148a3f6abf2df45de4f28a823c719586
coderyeasin <yeasin3233@gmail.com>
2026-01-18 02:18:50 +0600

    done-footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,11 +2,14 @@ import React from "react";
 
 import { FaPhoneVolume } from "react-icons/fa";
 import { TbWorldWww } from "react-icons/tb";
-
+import fb from "/icons/fb.png";
+import insta from "/icons/insta.png";
+import twt from "/icons/twt.png";
+import linkdn from "/icons/linkdn.png";
 const Footer = () => {
   return (
     <>
-      <footer className="w-full h-152.25 bg-TextPrimary py-10">
+      <footer className="w-full h-152.25 bg-TextPrimary py-18 space-y-12">
         <div className="text-white flex justify-around items-center">
           <p className="font-inter font-medium text-[24px] leading-10">
             44, Big Building Roosevelt Street <br />
@@ -23,10 +26,13 @@ const Footer = () => {
             </p>
           </div>
         </div>
-        <div className="border-b-8 border-secondary border-t-8 border-secondary py-20">
-          <h3 className="text-white">icons</h3>
+        <div className="border-b border-t border-slate-800 py-20 flex justify-center items-center gap-5 max-w-330 mx-auto">
+          <img src={fb} alt="fb" className="w-16 h-16 cursor-pointer" />
+          <img src={insta} alt="insta" className="w-16 h-16 cursor-pointer" />
+          <img src={twt} alt="twt" className="w-16 h-16 cursor-pointer" />
+          <img src={linkdn} alt="linkdn" className="w-16 h-16 cursor-pointer" />
         </div>
-        <div className="text-white font-inter font-medium text-[24px] flex justify-around items-center">
+        <div className="text-white font-inter font-medium text-[24px] flex justify-between items-center max-w-330 mx-auto">
           <h3>2027, All Rights Reserved.</h3>
           <h3>Unsubscribe</h3>
         </div>
